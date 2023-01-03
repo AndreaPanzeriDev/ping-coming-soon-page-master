e3f8d775552ee5ede519c0a67ccaeee8042f9f63
AndreaPanzerDev <andpanz@libero.it>
2023-01-03 20:30:03 +0100

    finish

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,7 +4,7 @@
 function check() {
     let email = document.getElementById('clientMail').value;
     console.log(email);
-    if(email.includes('.')){
+    if(email.includes('.') && email.includes('@')){
         console.log('ok');
         document.getElementById('clientMail').style.border="1px solid hsl(223, 100%, 88%)";
         document.getElementById('error').style.display="none";
