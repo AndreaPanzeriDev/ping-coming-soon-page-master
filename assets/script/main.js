083f6e2b7988da7c989824b7b32d17995cb68dd3
AndreaPanzerDev <andpanz@libero.it>
2023-01-03 20:00:23 +0100

    auto-save

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,16 @@
 
-let email = document.getElementById('clientMail').value;
+
 
 function check() {
+    let email = document.getElementById('clientMail').value;
     console.log(email);
     if(email.includes('.')){
         console.log('ok');
+        document.getElementById('clientMail').style.border="1px solid hsl(223, 100%, 88%)";
+        document.getElementById('error').style.display="none";
     }else{
         document.getElementById('clientMail').style.border="1px solid hsl(354, 100%, 66%)";
+        document.getElementById('error').style.display="block";
         console.log('problem');
     }
 
